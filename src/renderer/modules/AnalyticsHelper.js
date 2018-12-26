b3c6fada4257c363a3ac477d0ec0db844f3e3b1e
Joo <joo@joostory.net>
2018-12-26 15:46:59 +0900

    electron-ga로 변경

--- src/renderer/modules/AnalyticsHelper.js
+++ src/renderer/modules/AnalyticsHelper.js
@@ -1,34 +1,19 @@
-import Analytics from 'electron-google-analytics'
+import Analytics from 'electron-ga'
 import uuidV4 from 'uuid/v4'
 
 const clientID = uuidV4()
-const analytics = new Analytics('UA-26767980-11')
+const analytics = new Analytics('UA-26767980-11', {
+  userId: clientID
+})
 
 export const pageview = (page, pageTitle) => {
 	if (process.env.NODE_ENV !== 'production') {
 		return
-	}
-
-	return analytics
-	.pageview('http://tistory-editor.joostory.net', page, pageTitle, clientID)
-	.then((response) => {
-    return response
-  }).catch((err) => {
-    return err
-	})
-}
-
-export const event = (category, action) => {
-	if (process.env.NODE_ENV !== 'production') {
-		return
-	}
-	
-	return analytics
-	.event(category, action, { clientID })
-	.then((response) => {
-    return response
-  }).catch((err) => {
-    return err
-	})
+  }
+  
+  return analytics.send('pageview', {
+    dl: encodeURIComponent(page),
+    dt: pageTitle
+  })
 }
 
