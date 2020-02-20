f8d0baedff7453e3239265979b06e3a19db02a05
Joo <joo@joostory.net>
2020-02-20 23:50:48 +0900

    sidebar header ui 변경

--- src/renderer/components/blog/sidebar/Header.js
+++ src/renderer/components/blog/sidebar/Header.js
@@ -1,41 +1,61 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import {
-  Toolbar, IconButton, Avatar, Typography, Button,
-  makeStyles
+  Toolbar, Avatar, Typography, Button,
+  makeStyles,
+  Badge, IconButton
 } from '@material-ui/core'
+import { Menu, MoreHoriz, MoreVert } from '@material-ui/icons'
+import Providers from '../../../constants/Providers'
 
 const useStyle = makeStyles(theme => ({
   root: {
     paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2)
+    paddingRight: theme.spacing(1)
   },
   title: {
     flexGrow: 1,
     paddingLeft: theme.spacing(2)
+  },
+  avatar: {
+    width: theme.spacing(5),
+    height: theme.spacing(5)
+  },
+  smallAvatar: {
+    width: theme.spacing(2),
+    height: theme.spacing(2)
   }
 }))
 
 function ProfileAvatar({blog}) {
-	if (blog.avatar) {
-		return <Avatar src={blog.avatar[1].url} size={30} />
+  const classes = useStyle()
+	if (blog.image) {
+		return <Avatar src={blog.image} className={classes.avatar} />
 	} else {
-		return <Avatar size={30}>{blog.title.slice(0,1)}</Avatar>
+		return <Avatar className={classes.avatar}>{blog.title.slice(0,1)}</Avatar>
 	}
 }
 
 export default function Header({onSelectBlog}) {
   const classes = useStyle()
+  const currentAuth = useSelector(state => state.currentAuth)
   const currentBlog = useSelector(state => state.currentBlog)
+  const provider = Providers.find(p => p.name == currentAuth.provider)
 
 	return (
 		<Toolbar className={classes.root}>
-			<ProfileAvatar blog={currentBlog} />
+      <Badge
+        overlap='circle'
+        anchorOrigin={{vertical:'bottom',horizontal:'right'}}
+        badgeContent={<Avatar src={provider.logo} className={classes.smallAvatar} />}
+      >
+        <ProfileAvatar blog={currentBlog} />
+      </Badge>
 
 			<Typography className={classes.title}>
 				{currentBlog.title}
 			</Typography>
-      <Button onClick={onSelectBlog}>선택</Button>
+      <IconButton onClick={onSelectBlog}><MoreHoriz /></IconButton>
 		</Toolbar>
 	)
 }
