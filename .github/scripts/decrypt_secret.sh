0138f72cb3449675faf15748d948185103c77d78
Joo <joo@joostory.net>
2021-01-23 22:09:07 +0900

    build secret change

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $BUILD_KEY -iv $BUILD_IV -a \
-  -in oauthInfo.json.ssl -out oauthInfo.json -d
+openssl aes-256-cbc -pass $BUILD_PASS -iter 2 -a -d -in oauthInfo.json.ssl -out oauthInfo.json
