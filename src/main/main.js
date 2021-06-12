f5c890eec2c9b7e57efcd66c913ea70f88e82028
Joo <joo@joostory.net>
2021-06-12 21:06:08 +0900

    변경된 open-url로 macos도 동작하도록 수정

--- src/main/main.js
+++ src/main/main.js
@@ -6,14 +6,15 @@ const OAuthRequestManager = require('./oauth/OAuthRequestManager')
 
 const PROTOCOL = "tistory-editor"
 
+console.log("DEBUG: platform", process.platform)
 settings.configure({
   fileName: 'Settings'
 })
 
 let deeplinkingUrl
 app.setAsDefaultProtocolClient(PROTOCOL)
-
 app.showExitPrompt = false
+
 app.on('ready', () => {
   initWindow()
   ipc.init()
@@ -29,32 +30,48 @@ app.on('window-all-closed', () => {
 
 app.on("open-url", (e, urlString) => {
   e.preventDefault()
-  deeplinkingUrl = urlString
+  
+  if (process.platform === 'darwin') {
+    execOAuthRequestHandler(urlString)
+  } else {
+    deeplinkingUrl = urlString
+  }
 })
 
+function execOAuthRequestHandler(deeplinkingUrl) {
+  console.log("OPEN-URL", deeplinkingUrl)
+  const url = new URL(deeplinkingUrl)
+  let requestHandler = OAuthRequestManager.loadRequestInfo("oauth")
+  if (requestHandler) {
+    requestHandler(url.searchParams)
+  }
+}
+
+function restoreWindow() {
+  const window = getWindow()
+  if (!window) {
+    return
+  }
+  if (window.isMinimized()) {
+    window.restore()
+  }
+
+  window.focus();
+}
+
 const gotTheLock = app.requestSingleInstanceLock();
+console.log("DEBUG: gotTheLock", gotTheLock)
 if (!gotTheLock) {
-  app.quit();
-  return;
+  app.quit()
 } else {
   app.on('second-instance', (e, argv) => {
-    if (process.platform === 'darwin') {
+    console.log("DEBUG: second-instance argv", argv)
+    if (process.platform !== 'darwin') {
       deeplinkingUrl = argv.find((arg) => arg.startsWith('tistory-editor://'));
     }
 
-    const window = getWindow()
-
-    if (window) {
-      if (window.isMinimized()) window.restore();
-      window.focus();
-
-      console.log("OPEN-URL", deeplinkingUrl)
-      const url = new URL(deeplinkingUrl)
-      let requestHandler = OAuthRequestManager.loadRequestInfo("oauth")
-      if (requestHandler) {
-        requestHandler(url.searchParams)
-      }
-    }
-  });
+    restoreWindow()
+    execOAuthRequestHandler(deeplinkingUrl)
+  })
 }
 
